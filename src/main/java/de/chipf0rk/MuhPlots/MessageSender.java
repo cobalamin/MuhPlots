db4b2409bfba141a3808e153349eb90aa1a117dc
Simon Welker <simon.welker@esirion.de>
2015-04-14 08:35:50 +0200

    Add help to MessageSender

--- src/main/java/de/chipf0rk/MuhPlots/MessageSender.java
+++ src/main/java/de/chipf0rk/MuhPlots/MessageSender.java
@@ -1,5 +1,8 @@
 package de.chipf0rk.MuhPlots;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
@@ -10,6 +13,22 @@ public class MessageSender {
 	MessageSender(MuhPlots plugin) {
 		this.plugin = plugin;
 	}
+	
+	private String helpEntry(String cmd, String explanation) {
+		return " " + ChatColor.BLUE + " > " + ChatColor.DARK_AQUA +
+			"/mp " + cmd +
+			ChatColor.BLUE + " > " + ChatColor.WHITE + explanation;
+	}
+	
+	List<String> help = Arrays.asList(
+		helpEntry("protect", "Protects your plot"),
+		helpEntry("add", "Adds a friend to your plot"),
+		helpEntry("rem", "Removes a friend from your plot"),
+		helpEntry("info", "Info about the current plot"),
+		helpEntry("list", "Lists all your plots"),
+		helpEntry("tp", "Teleports you to a plot"),
+		helpEntry("find", "Finds an empty plot")
+	);	
 	
 	private String formatString(State state, String string) {
 		switch(state) {
@@ -35,5 +54,13 @@ public class MessageSender {
 	void send(CommandSender recipient, State state, String message) {
 		String formattedMessage = formatString(state, message);
 		recipient.sendMessage(formattedMessage);
+	}
+	
+	void sendHelpTo(CommandSender recipient) {
+		recipient.sendMessage("");
+		for(String line : help) {
+			recipient.sendMessage(line);
+		}
+		recipient.sendMessage("");
 	}
 }
